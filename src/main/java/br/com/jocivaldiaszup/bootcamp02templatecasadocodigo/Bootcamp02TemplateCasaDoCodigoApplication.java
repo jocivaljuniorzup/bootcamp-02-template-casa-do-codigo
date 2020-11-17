dd4e118fbd1ab30c0d4e02c7539418539c487584
Jocival Dantas Dias Junior <jocival.junior@zup.com.br>
2020-11-17 20:39:47 -0300

    Commit Inicial do Projeto - Atividade: 0-0-0-setup

---
+++ src/main/java/br/com/jocivaldiaszup/bootcamp02templatecasadocodigo/Bootcamp02TemplateCasaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.jocivaldiaszup.bootcamp02templatecasadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp02TemplateCasaDoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp02TemplateCasaDoCodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jocivaldiaszup/bootcamp02templatecasadocodigo/Bootcamp02TemplateCasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jocivaldiaszup.bootcamp02templatecasadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp02TemplateCasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
